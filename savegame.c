00b74fdce1599c5936f785bb0b55a405f6a4cfd8
Ashish kumar <111566861+kmrasis@users.noreply.github.com>
2022-08-22 21:50:30 +0530

    Add files via upload

---
+++ savegame.c
@@ -0,0 +1,11 @@
+#include <windows.h>
+#include <Mmsystem.h>
+
+
+int locate(int x,int y)
+{
+    COORD coord={0,0};
+    coord.X=x;
+    coord.Y=y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+}
